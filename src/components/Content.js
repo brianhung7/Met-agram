93f909661f24580a662a1d0d3ca6b8537a48b74b
brianhung7 <brian.texts@gmail.com>
2022-01-22 15:15:58 -0800

    Fetch request working

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,32 @@ const{useState, useEffect} = React;
 
 
 const Content = () => {
+    const [content, setContent] = useState(null)
+
+    const api = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/436121'
+
+    const checkErrors = (res) => {
+        if(!res.ok){
+            throw Error(res.statusText)
+        }
+        return res
+    }
+    const fetchData = async () => {
+        await fetch(api)
+            .then(checkErrors)
+            .then(response => response.json())
+            .then(data => {
+                setContent(data)
+                console.log(data)
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchData();
+
+    },[])
+
 
     return (
         <>
