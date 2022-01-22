e6723b58f4aee8b8f23d0fa37cf07ccd0101b77d
brianhung7 <brian.texts@gmail.com>
2022-01-22 14:53:11 -0800

    Content Component setup

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const{useState, useEffect} = React;
+
+
+const Content = () => {
+
+    return (
+        <>
+            Yay!
+        </>
+    )
+}
+
+export default Content
