7b2461eaa9a596c57c30f1089f0f02673bfe1a94
brianhung7 <brian.texts@gmail.com>
2022-01-22 23:23:48 -0800

    Added small hover animation on like button

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Typography, Card, CardMedia } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Message from '../Message/Message';
-import { artDesign } from './ArtStyles';
-
+import { artDesign, favoriteIconStyle, unfavoriteIconStyle } from './ArtStyles';
 
 
 const Art = ({content, messages, setMessages, like, setLike}) => {
@@ -17,8 +16,8 @@ const Art = ({content, messages, setMessages, like, setLike}) => {
                     <Typography>By: {content.artist} </Typography>
                 </CardMedia>
                 <CardMedia image={content.image} component='img' alt="Met Museum" />
-                {like ? <FavoriteIcon fontSize="large" cursor="pointer" onClick={() => setLike(false)} sx={{ color: 'red' }}/> : 
-                <FavoriteIcon fontSize="large" cursor="pointer" onClick={() => setLike(true)}/> }
+                {like ? <FavoriteIcon fontSize="large" cursor="pointer" onClick={() => setLike(false)} sx={favoriteIconStyle}/> : 
+                <FavoriteIcon fontSize="large" cursor="pointer" onClick={() => setLike(true)} sx={unfavoriteIconStyle}/> }
                 <Message messages={messages} setMessages={setMessages}  />
             </Card>
         </>
--- src/components/Art/ArtStyles.js
+++ src/components/Art/ArtStyles.js
@@ -4,3 +4,17 @@ export const artDesign = {
     boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset',
     width: '60%',
 }
+
+export const favoriteIconStyle = {
+    color: 'red',
+    '&:hover': {
+        transition: 'all .3s ease-in-out',
+        transform: 'scale(1.2)',
+    }
+}
+export const unfavoriteIconStyle = {
+    '&:hover': {
+        transition: 'all .3s ease-in-out',
+        transform: 'scale(1.2)',
+    }
+}
