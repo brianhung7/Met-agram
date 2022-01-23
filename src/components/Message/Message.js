7ec23ca8a630167044d9a86077db675437c464a7
brianhung7 <brian.texts@gmail.com>
2022-01-23 11:54:58 -0800

    Delete message functionality

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,12 +2,14 @@ import React from "react";
 import { CardMedia, Typography, Box, TextField, Button } from "@mui/material";
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import { formStyle } from "./MessageStyles";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 const {useState} = React;
 
 const Comment = ({messages, setMessages}) => {
     const [text, setText] = useState('')
 
-    const updateMessages = (event) => {
+    const addMessage = (event) => {
         //Stop page from refreshing
         event.preventDefault()
         //make temp message array and push new text in then update
@@ -18,21 +20,31 @@ const Comment = ({messages, setMessages}) => {
         setText('')
         event.target[0].value = ''
     }
+
+    const deleteComment = (index) =>{
+        let tempMessages = [...messages]
+        //Removing selected message from array
+        tempMessages.splice(index, 1)
+        setMessages(tempMessages)
+    }
+
     return (
         <CardMedia>
-            {messages?.map((comment) => (
+            {messages?.map((comment, index) => (
                 <CardMedia sx={{display:'flex', justifyContent:'center'}}>
+                    <AccountCircleIcon />
                     <Typography sx={{fontWeight:'bold'}}>
                         MetLover  
                     </Typography>
                     <Typography>
                        : {comment}
                     </Typography>
+                    <DeleteOutlineIcon sx={{ color: 'red' }} cursor="pointer" onClick={() => deleteComment(index)}/>
                 </CardMedia>
             ))}
-            <Box component="form" onSubmit={updateMessages} sx={formStyle}>
+            <Box component="form" onSubmit={addMessage} sx={formStyle}>
                 <TextField id="outlined-basic" label="Leave a Message!" variant="outlined" onInput={(event) => setText(event.target.value)}/>
-                <Button endIcon={<AddCommentIcon />} type='submit' variant='contained'>Send</Button>
+                <Button disabled={text === ''}endIcon={<AddCommentIcon />} type='submit' variant='contained'>Send</Button>
             </Box>
         </CardMedia>
     )
